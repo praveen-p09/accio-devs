f91385a5ffc76a312d1ea5a5017c5a019104a088
Harsh Pastor <harshpastor@gmail.com>
2023-11-01 12:22:54 +0530

    Update dataTable.jsx

--- src/Components/ComplaintData/dataTable.jsx
+++ src/Components/ComplaintData/dataTable.jsx
@@ -1,5 +1,7 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { addLocationtoDb, getCoordinate } from '../utilities/Firebase'
+
 
 function TabularData() {
   const [data, setData] = useState([]);
@@ -28,10 +30,15 @@ function TabularData() {
             <tr>
               <th>State</th>
               <th>District</th>
-              <th>Area</th>
-              <th>Contact</th>
+              <th>City</th>
+              <th>Address</th>
               <th>Name</th>
+              <th>Contact</th>
+              <th>Email</th>
               <th>Severity</th>
+              <th>Pincode</th>
+              <th>Message</th>
+              <th>Image</th>
             </tr>
           </thead>
           <tbody>
@@ -39,10 +46,14 @@ function TabularData() {
               <tr key={index}>
                 <td>{item.state}</td>
                 <td>{item.district}</td>
-                <td>{item.area}</td>
-                <td>{item.contact}</td>
+                <td>{item.city}</td>
+                <td>{item.address}</td>
                 <td>{item.name}</td>
+                <td>{item.contact}</td>
+                <td>{item.email}</td>
                 <td>{item.severity}</td>
+                <td>{item.pincode}</td>
+                <td>{item.image}</td>
               </tr>
             ))}
           </tbody>
