77ccfc483289237b2eb221e63799c79545798b57
Praveen Patro <praveen.patro911@gmail.com>
2023-11-02 03:47:13 +0530

    Update home+added navbar

--- src/App.jsx
+++ src/App.jsx
@@ -6,12 +6,12 @@ import {
   Routes,
 } from "react-router-dom";
 import Addlocation from './Components/Addlocation'
-import MapContainer from '../src/page/mainMap/Map';
+import Home from '../src/Components/Home/Home';
 import ImageUploadForm from './Components/ComplaintForm/form';
 import Pothole from './Components/pothole/pothole';
 import IframeComponent from './Components/stream/stream';
 import TabularData from './Components/ComplaintData/dataTable';
-
+import MapContainer from '../src/page/mainMap/Map';
 function App() {
 
 
@@ -21,12 +21,12 @@ function App() {
      
             <Routes>
               <Route exact path="/test" element={ <Addlocation/> } />
-              <Route exact path="/" element={  <MapContainer/>  } />
+              <Route exact path="/" element={  <Home/>  } />
               <Route path="/form" element={<ImageUploadForm/>} />
               <Route path="/pothole/:holeID" element={<Pothole/>} />
               <Route path="/stream" element={<IframeComponent/>} />
               <Route path="/formdisplay" element={<TabularData/>} />
-             
+              <Route path="/map" element={<MapContainer/>} />
             </Routes>
           
         </Router>
---
+++ src/Components/Home/Home.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import Navbar from '../Navbar/navbar';
+function Home() {
+
+
+    return (
+      <>
+          <Navbar/>
+          <h1>Home</h1>
+      </>
+    )
+  }
+  
+  export default Home;
+  
