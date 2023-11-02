07f199bc6aa66c9dd4fc94d3e8bf066dfbd1054c
Praveen Patro <praveen.patro911@gmail.com>
2023-11-02 13:45:07 +0530

    Final commit

--- src/Components/Home/Home.jsx
+++ src/Components/Home/Home.jsx
@@ -1,11 +1,12 @@
 import React from 'react';
+import './Home.css'
 function Home() {
 
 
     return (
       <>
-         
-          <h1>Home</h1>
+          <h1>Pothole Detection using ML Model</h1>
+          
       </>
     )
   }
--- src/Components/pothole/pothole.jsx
+++ src/Components/pothole/pothole.jsx
@@ -1,68 +1,66 @@
 import React, { useEffect, useState } from 'react'
 import { useParams,useNavigate } from "react-router-dom";
-import { getCoordinate } from '../../utilities/Firebase'
-import Navbar from '../Navbar/navbar';
+import {getCoordinate } from '../../utilities/Firebase'
+// import Navbar from '../Navbar/navbar';
+// import ProgressBar from 'react-bootstrap/ProgressBar';
 import CommentCard from '../Comments/comments';
 import { signInWithGoogleRedirect, signUserAccountOut } from "../../utilities/Firebase";
 import './pothole.css';
-const ProgressBarWithSteps = ({ steps, currentStep }) => {
-  const stepWidth = (100 / (steps.length - 1)) * currentStep;
-}
 const Pothole = () => {
   const postId = useParams();
-  console.log(postId)
+  // console.log(postId)
   const [locfromdb, setlocfromdb] = useState([]);
-  const [holedetail, setholedetail] = useState();
+const [holedetail, setholedetail] = useState();
   const [hole, sethole] = useState();
 
-  const navigate=useNavigate();
+  const navigate=useNavigate();  
 
-
-
+  
 
   useEffect(() => {
     const getloca = async () => {
-      const data = await getCoordinate();
-      setlocfromdb(data);
-      const postInfo = data.filter((x) => x.id === postId.holeID)[0];
-      console.log(postInfo);
-      setholedetail(postInfo)
-      console.log(holedetail)
-      // console.log(data);
+        const data = await getCoordinate();
+        setlocfromdb(data);
+        const postInfo = data.filter((x) => x.id === postId.holeID)[0];
+        console.log(postInfo);
+        setholedetail(postInfo)
+        console.log(holedetail)
+        // console.log(data);
     }
-
+    
     getloca();
+   
+    
+}, [])
+const handlesignout = async () => {
+  await signUserAccountOut();
+  localStorage.clear();
+  navigate("/map");
+
+}
 
 
-  }, [])
-  const handlesignout = async () => {
-    await signUserAccountOut();
-    localStorage.clear();
-    navigate("/map");
+const signInWithGoogle = async () => {
+  const response = await signInWithGoogleRedirect();
+  if (response) {
+    localStorage.setItem("isAuth", true);
 
+    console.log(localStorage.getItem("isAuth"));
+
+    navigate(`/pothole/${postId}`);
   }
 
-
-  const signInWithGoogle = async () => {
-    const response = await signInWithGoogleRedirect();
-    if (response) {
-      localStorage.setItem("isAuth", true);
-
-      console.log(localStorage.getItem("isAuth"));
-
-      navigate(`/pothole/${postId}`);
-    }
-
-  };
-  const signinhandle = () => {
-    signInWithGoogle();
-    // localStorage.setItem("isAuth");
-  };
+};
+const signinhandle = () => {
+  signInWithGoogle();
+  // localStorage.setItem("isAuth");
+};
 
 
   return (
     <>
-
+      
+      
       <div className="pothole">
     {!localStorage.getItem('isAuth')?<div> <button
             onClick={signinhandle}
@@ -80,37 +78,46 @@ const Pothole = () => {
 
         { holedetail && (
 
-          <div className="container">
-            <h1>Pothole -{ holedetail.id }</h1>
-            <div className='flexr'>
-              <div className='bg3d wrapper'>
+       
+      <div className="container">
+         <h1>Pothole -{holedetail.id}</h1>
+         <div className='flexr'>
+        <div className='bg3d wrapper'>
 
-                <img src="https://t3.ftcdn.net/jpg/03/60/90/92/360_F_360909266_3mWP6FZZOQMG5aRUozFYPKjPm6FK4nUx.jpg" alt="bg-scale" />
-                <model-viewer id="reveal" loading="eager" src={ `../../model.glb` } alt="A 3D model of an item" camera-controls></model-viewer>
-              </div>
-              <div className="details">
-                <h3>Details</h3>
-                <p>Coordinates : { holedetail.longitude }, { holedetail.latitude }</p>
-                <p>Area:{ holedetail.area } m square</p>
-                <p>Volume :{ holedetail.volume } m cube</p>
-                <p>Severity :High</p>
-                <p>Material : Asphalt concrete</p>
-                <p>Suggestion : </p>
-                <ul>
-                  <li><p>Sealing</p></li>
-                  <li><p>Overlays</p></li>
-                  <li><p>Reconstruction</p></li>
-                  <li><p>Patching</p></li>
-                </ul>
-
-              </div>
-            </div>
-          </div>) }
+          <img src="https://t3.ftcdn.net/jpg/03/60/90/92/360_F_360909266_3mWP6FZZOQMG5aRUozFYPKjPm6FK4nUx.jpg" alt="bg-scale" />
+          <model-viewer id="reveal" loading="eager" src={`../../model.glb`} alt="A 3D model of an item" camera-controls></model-viewer>
+        </div>
+        <div  className="details">
+          <h3>Details</h3>
+          <p>Coordinates : {holedetail.longitude}, {holedetail.latitude}</p>
+          <p>Area:{holedetail.area} m square</p>
+          <p>Volume :{holedetail.volume} m cube</p>
+          <p>Severity :High</p>
+          <p>Material : Asphalt concrete</p>
+          <p>Suggestion : </p>
+          <ul>
+            <li><p>Sealing</p></li>
+            <li><p>Overlays</p></li>
+            <li><p>Reconstruction</p></li>
+            <li><p>Patching</p></li>
+          </ul>
+          
+        </div>
+        </div>
+      </div>)}
       </div>
       <h1>Comments by Experts</h1>
-      <CommentCard />
-
-
+      <CommentCard/>
+      
+      {/* <ProgressBar animated now={60}  label={`{60}%`}/>
+      
+        <p><img src={require("../../assets/green_checkmark.svg").default} alt="tick" /> Form submitted and Drone review</p>
+        <p><img src={require("../../assets/green_checkmark.svg").default} alt="tick" /> Sent to Chief Engineer, PWD</p>
+        <p><img src={require("../../assets/green_checkmark.svg").default} alt="tick" /> Material and budget sanctioned, assigned to job Engineer</p>
+        <p><img src={require("../../assets/pending.svg").default} alt="pending" /> Road maintenance contractor tender passed</p>
+        <p><img src={require("../../assets/pending.svg").default} alt="pending" /> Job executed (Delete record)</p>
+       */}
+      
     </>
   )
 }
