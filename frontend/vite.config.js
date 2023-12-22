2f49fdc63a18ded3b52f8e1708869609152705d0
Praveen Patro <128630671+praveen-p09@users.noreply.github.com>
2023-12-22 22:21:17 +0530

    docker update

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
-// https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-})
+  server: {
+    host: true,
+    port: 5173, // Port used by docker container
+    strictPort: true,
+     watch: {
+       usePolling: true
+     }
+  }
+ })
