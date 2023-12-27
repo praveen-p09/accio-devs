5a1b15099d33c9a98355c480fd5c4df915fece8a
Ayush shukla <ayushshukla3999@gmail.com>
2023-12-27 18:41:21 +0530

    upload video

--- frontend/src/Components/stream/stream.jsx
+++ frontend/src/Components/stream/stream.jsx
@@ -1,9 +1,25 @@
 // import React from 'react';
 // import './stream.css';
 
-import React from 'react';
+import React,{ Component }  from 'react';
 
 class VideoContainer extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      ipAddress: "http://192.168.137.3:4000",
+    };
+  }
+   change=(e)=>{
+      this.setState({
+        ipAddress: e.target.value,
+      })
+      console.log(this.state.ipAddress);
+  }
+  
+
+
+
   render() {
     const containerStyle = {
       width: '100vw',  // Set the width to 25% of the viewport width
@@ -18,15 +34,21 @@ class VideoContainer extends React.Component {
     };
 
     return (
+      <div>
       <div style={containerStyle}>
+         <h1>Stream</h1>
+         <form action="">
+         <div> Enter your IpAddress : <input type="text" value={this.state.ipAddress} onChange={this.change}/></div> 
+        </form>
         <iframe
           title="Embedded Video"
-          src="http://192.168.137.3:4000" // Replace with your actual URL
+          src={this.state.ipAddress} // Replace with your actual URL
           frameBorder="0"
           style={iframeStyle}
           allowFullScreen
         ></iframe>
-        <h1>Stream</h1>
+       
+      </div>
       </div>
     );
   }
